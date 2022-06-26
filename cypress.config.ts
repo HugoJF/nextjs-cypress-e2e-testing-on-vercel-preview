06790e0066ac88dc01cd500deb6de8838984854e
Hugo Jeller <hugo_jeller@hotmail.com>
2022-06-26 16:44:09 -0400

    GitHub Actions setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   video: false,
   e2e: {
+    baseUrl: process.env.E2E_BASE_URL || "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
