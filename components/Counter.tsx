172acb6a8e6595023ba6429d73e2f0a4ff7506bc
Hugo Jeller Ferreira <hugo_jeller@hotmail.com>
2022-06-26 16:47:08 -0400

    Improve border colors

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -11,22 +11,22 @@ export default function Counter({initial = 0, increments = 1}) {
         setCount(_count => _count - increments)
     }
 
-    return <div className="flex divide-x border rounded-lg">
+    return <div className="flex divide-x border divide-gray-700 border-gray-700 font-mono rounded-lg">
         <button
-            className="duration-150 p-8 hover:bg-gray-800 rounded-l-lg"
+            className="duration-150 p-8 text-gray-300 hover:text-gray-100 hover:bg-gray-800 rounded-l-lg"
             onClick={handleDecrement}
             data-test="decrement"
         >
             -
         </button>
         <span
-            className="py-8 px-16 font-mono"
+            className="py-8 px-16 text-gray-100"
             data-test="count"
         >
             {count}
         </span>
         <button
-            className="duration-150 p-8 hover:bg-gray-800 rounded-r-lg"
+            className="duration-150 p-8 text-gray-300 hover:text-gray-100 hover:bg-gray-800 rounded-r-lg"
             onClick={handleIncrement}
             data-test="increment"
         >
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Counter from "../components/Counter";
 
 const Home: NextPage = () => {
 
-    return <main className="flex items-center justify-center min-h-screen text-5xl text-white bg-gray-900">
+    return <main className="flex items-center justify-center min-h-screen text-5xl bg-gray-900">
         <Counter
             increments={10}
         />
